f6cbfd7cf08d691e55fcf787bec3b8f396bf26d2
Madhav008 <madhavj211@gmail.com>
2022-08-16 18:26:40 +0530

    Change the title

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React From Madhav
+        </a> 
       </header>
     </div>
   );
